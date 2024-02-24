1b1c707b71b8b4cca25303240c0f40e7c6727710
Mohan Bora <mbora@periandx.com>
2024-02-24 20:57:54 +0530

    uploaded with parcel

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # This is basic of React from scratch 🍿
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching Faster Builds
+- Image Optimization
+- Minification of files for prod builds
+- Bundling
+- compress files
+- Consistent Hasing
+- Code Splitting
+- Differential Bundling - support older version of browsers
+- Diagnostics
+- Error Handling
+- Https
+- Tree shaking - remove unused code
+- prod deployment - npx parcel build index.html (delete main:"app.js" from package.json )
--- app.js
+++ app.js
@@ -1,20 +1,5 @@
-/**
- * <div id="parent">
- *      <div id="child1">
- *          <h1>Heading 1</h1>
- *          <h2>Heading 2</h2>
- *      </div>
- *      <div id="child2">
- *          <h1>Heading 1</h1>
- *          <h2>Heading 2</h2>
- *      </div>
- * </div>
- */
-
-
-// const heading =  React.createElement("h1", { id:"heading", xyz:"abc"}, "Hello World from React!");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+import React  from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement("div", {id:"parent"}, [
     React.createElement("div", {id: "child1"}, [
