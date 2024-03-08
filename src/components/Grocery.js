0854620b7d488024ac8adceea1c975fef9b48b9d
Mohan Bora <mbora@periandx.com>
2024-03-08 19:00:52 +0530

    lazy load

---
+++ src/components/Grocery.js
@@ -0,0 +1,5 @@
+const Grocery = () => {
+    return <h1>This is a grocery store that will be build with different business usecase. Will use Lazy loading for code chunking!!</h1>
+}
+
+export default Grocery;
